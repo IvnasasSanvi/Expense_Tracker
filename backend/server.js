f06b1a3bade619004b31413814a6154a2f4da750
SANVI <sanvijsr1@gmail.com>
2026-03-25 01:08:19 +0530

    expense planner

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+const app = express();
+const port = 4000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+//db
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('API WORKING');
+});
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
