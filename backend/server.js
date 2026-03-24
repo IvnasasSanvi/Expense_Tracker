ff7bddb7cd11deddd47da57ddf736838780649a5
SANVI <sanvijsr1@gmail.com>
2026-03-25 01:24:01 +0530

    expense planner -backend

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ const port = 4000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 //db
 
 // Routes
@@ -15,5 +17,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port,()=>{
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port http://localhost:${port}`);
 })
