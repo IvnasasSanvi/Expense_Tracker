511a4134c9fbe5bc5a1a5f49baa7a26db94f3ebe
SANVI <sanvijsr1@gmail.com>
2026-03-25 18:14:09 +0530

    expense planner -backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,10 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-};
+
+    await mongoose.connect("mongodb+srv://sanvijsr1_db_user:eWDzXqLizCloSAcS@cluster0.u9avbrs.mongodb.net/Expense")
+    .then(() => {
+        console.log("Connected to MongoDB");
+    } )
+
+}
