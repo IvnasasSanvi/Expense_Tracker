373665fd77dd98c218e8791b7ca00c0790535e2b
SANVI <sanvijsr1@gmail.com>
2026-04-01 22:35:05 +0530

    to update a user profile

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,3 +2,8 @@ import User from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
 import dotenv from "dotenv";
 
+export default async function authMiddleware(req,res,next) {
+    //grab the token
+    const authHeader = req.headers.authoriztion;
+    
+}
