c4820836a9d4e82cc68496633e4cdb76dc1db79e
SANVI <sanvijsr1@gmail.com>
2026-04-07 23:29:23 +0530

    income Route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import 'dotenv/config';
 import { connectDB } from './config/db.js';
 import userRouter from './routes/userRoute.js';
+import incomeRouter from './routes/incomeRoute.js';
 const app = express();
 const port = 4000;
 
@@ -16,6 +17,7 @@ connectDB();
 
 // Routes
 app.use("/api/user", userRouter)
+app.use("/api/income", incomeRouter)
 
 app.get('/', (req, res) => {
     res.send('API WORKING');
