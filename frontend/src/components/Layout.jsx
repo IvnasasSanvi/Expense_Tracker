ca185c1b7e60189ce8959b44c739e2a5f4e53366
SANVI <sanvijsr1@gmail.com>
2026-04-24 23:19:57 +0530

    frontend

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { styles } from '../assets/dummyStyles'
 import Navbar from './Navbar'
+import { Sidebar } from './Sidebar'
 
 const Layout = ({onLayout, user}) => {
   return (
     <div className={styles.layout.root}>
       <Navbar user={user} onLayout={onLayout} />
+      <Sidebar/>
     </div>
   )
 }
