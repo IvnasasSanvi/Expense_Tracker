f42e8745d7492a62c912b2adb9b43e46acb9b4fb
SANVI <sanvijsr1@gmail.com>
2026-04-25 23:33:09 +0530

    frontend

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { styles } from '../assets/dummyStyles'
 import Navbar from './Navbar'
 import { Sidebar } from './Sidebar'
 
 const Layout = ({onLayout, user}) => {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState(false)
   return (
     <div className={styles.layout.root}>
       <Navbar user={user} onLayout={onLayout} />
-      <Sidebar/>
+      <Sidebar user={user} isCollapsed = {sidebarCollapsed} setIsCollapsed = {setSidebarCollapsed} /> 
     </div>
   )
 }
